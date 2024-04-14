73a19e4820b48daaf4dd48eb1ffd09bf8aaac6cb
Ing. Rosdanys Ramirez Arcas <rosdanys@gmail.com>
2024-04-14 19:19:54 -0400

    fix: error send email

--- src/libs/mail/sending.ts
+++ src/libs/mail/sending.ts
@@ -8,13 +8,16 @@ export async function sendMail({
   subject: string;
   body: string;
 }) {
-    const { NEXT_PUBLIC_SMTP_PASSWORD,NEXT_PUBLIC_SMTP_EMAIL_ADMIN, NEXT_PUBLIC_SMTP_EMAIL_ADMIN_FULLNAME} = process.env
+    const SMTP_PASSWORD = process.env.NEXT_PUBLIC_SMTP_PASSWORD
+    const SMTP_EMAIL_ADMIN = process.env.NEXT_PUBLIC_SMTP_EMAIL_ADMIN
+    const SMTP_EMAIL_ADMIN_FULLNAME = process.env.NEXT_PUBLIC_SMTP_EMAIL_ADMIN_FULLNAME
+    
     const transport = nodemailer.createTransport({
       service:"gmail",
       secure: true,
       auth:{
-        user:NEXT_PUBLIC_SMTP_EMAIL_ADMIN,
-        pass:NEXT_PUBLIC_SMTP_PASSWORD
+        user:SMTP_EMAIL_ADMIN,
+        pass:SMTP_PASSWORD
       }
     })
    /* try {
@@ -28,8 +31,8 @@ export async function sendMail({
 
    try {
     const sendResult = await transport.sendMail({
-      from:NEXT_PUBLIC_SMTP_EMAIL_ADMIN_FULLNAME,
-      to:NEXT_PUBLIC_SMTP_EMAIL_ADMIN,
+      from:SMTP_EMAIL_ADMIN_FULLNAME,
+      to:SMTP_EMAIL_ADMIN,
       subject,
       html:body
     })
