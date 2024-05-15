c5a4a6f438e5346479a4a016ad3cd09a1c2e78f2
Ing. Rosdanys Ramirez Arcas <rosdanys@gmail.com>
2024-05-14 20:11:27 -0400

    add phone in Contact Page

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -2,58 +2,94 @@ import { FormContact } from "../FormContact/FormContact";
 
 export default function Contact() {
   return (
-    <section id="contact" className="relative py-20 md:py-[120px]" data-aos="fade-in">
-  <div className="absolute top-0 left-0 -z-[1] w-full dark:bg-dark h-full"></div>
-  <div className="absolute top-0 left-0 -z-[1] h-1/2 w-full  dark:bg-dark-700 lg:h-[45%] xl:h-1/2"></div>
-  <div className="container px-4">
-    <div className="-mx-4 flex flex-wrap items-center">
-      <div className="w-full px-4 lg:w-7/12 xl:w-8/12">
-        <div className="ud-contact-content-wrapper">
-          <div className="ud-contact-title mb-12 lg:mb-[150px]">
-            <span className="mb-6 block text-base font-medium text-dark dark:text-white">
-              CONTACT US
-            </span>
-            <h2 className="max-w-[260px] text-[35px] leading-[1.14] font-semibold text-dark dark:text-white">
-              {`Let's talk about your problem.`}
-            </h2>
-          </div>
-          <div className="mb-12 flex flex-wrap justify-between lg:mb-0">
-            <div className="mb-8 flex w-[330px] max-w-full">
-              <div className="mr-6 text-[32px] text-primary">
-                <svg width="29" height="35" viewBox="0 0 29 35" className="fill-current">
-                  <path
-                    d="M14.5 0.710938C6.89844 0.710938 0.664062 6.72656 0.664062 14.0547C0.664062 19.9062 9.03125 29.5859 12.6406 33.5234C13.1328 34.0703 13.7891 34.3437 14.5 34.3437C15.2109 34.3437 15.8672 34.0703 16.3594 33.5234C19.9688 29.6406 28.3359 19.9062 28.3359 14.0547C28.3359 6.67188 22.1016 0.710938 14.5 0.710938ZM14.9375 32.2109C14.6641 32.4844 14.2812 32.4844 14.0625 32.2109C11.3828 29.3125 2.57812 19.3594 2.57812 14.0547C2.57812 7.71094 7.9375 2.625 14.5 2.625C21.0625 2.625 26.4219 7.76562 26.4219 14.0547C26.4219 19.3594 17.6172 29.2578 14.9375 32.2109Z" />
-                  <path
-                    d="M14.5 8.58594C11.2734 8.58594 8.59375 11.2109 8.59375 14.4922C8.59375 17.7188 11.2187 20.3984 14.5 20.3984C17.7812 20.3984 20.4062 17.7734 20.4062 14.4922C20.4062 11.2109 17.7266 8.58594 14.5 8.58594ZM14.5 18.4297C12.3125 18.4297 10.5078 16.625 10.5078 14.4375C10.5078 12.25 12.3125 10.4453 14.5 10.4453C16.6875 10.4453 18.4922 12.25 18.4922 14.4375C18.4922 16.625 16.6875 18.4297 14.5 18.4297Z" />
-                </svg>
+    <section
+      id="contact"
+      className="relative py-20 md:py-[120px]"
+      data-aos="fade-in"
+    >
+      <div className="absolute left-0 top-0 -z-[1] h-full w-full dark:bg-dark"></div>
+      <div className="absolute left-0 top-0 -z-[1] h-1/2 w-full  dark:bg-dark-700 lg:h-[45%] xl:h-1/2"></div>
+      <div className="container px-4">
+        <div className="-mx-4 flex flex-wrap items-center">
+          <div className="w-full px-4 lg:w-7/12 xl:w-8/12">
+            <div className="ud-contact-content-wrapper">
+              <div className="ud-contact-title mb-12 lg:mb-[150px]">
+                <span className="mb-6 block text-base font-medium text-dark dark:text-white">
+                  CONTACT US
+                </span>
+                <h2 className="max-w-[260px] text-[35px] font-semibold leading-[1.14] text-dark dark:text-white">
+                  {`Let's talk about your problem.`}
+                </h2>
               </div>
-              <div>
-                <h5 className="mb-[18px] text-lg font-semibold text-dark dark:text-white">Our Location</h5>
-                <p className="text-base text-body-color dark:text-dark-6">
-                  401 Broadway, 24th Floor, Orchard Cloud View, London
-                </p>
+              <div className="mb-12 flex flex-wrap justify-between lg:mb-0">
+                <div className="mb-8 flex w-[330px] max-w-full">
+                  <div className="mr-6 text-[32px] text-primary">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      width={36}
+                      height={36}
+                      color={"#f2782c"}
+                      fill={"none"}
+                    >
+                      <path
+                        d="M4.74038 14.3685L6.69351 12.9816C7.24445 12.5904 7.80305 12.3282 8.44034 12.1585C9.17201 11.9636 9.5 11.5644 9.5 10.711C9.5 8.54628 14.5 8.31594 14.5 10.711C14.5 11.5644 14.828 11.9636 15.5597 12.1585C16.202 12.3295 16.7599 12.5934 17.3065 12.9816L19.2596 14.3685C20.1434 14.9961 20.5547 15.2995 20.7842 15.7819C21 16.2358 21 16.768 21 17.8324C21 19.7461 21 20.703 20.4642 21.3164C19.8152 22.0593 18.128 21.9955 17.0917 21.9955H6.90833C5.87197 21.9955 4.21909 22.0986 3.5358 21.3164C3 20.703 3 19.7461 3 17.8324C3 16.768 3 16.2358 3.21584 15.7819C3.44526 15.2995 3.85662 14.9961 4.74038 14.3685Z"
+                        stroke="currentColor"
+                        strokeWidth="1.5"
+                      />
+                      <path
+                        d="M14 17C14 18.1046 13.1046 19 12 19C10.8954 19 10 18.1046 10 17C10 15.8954 10.8954 15 12 15C13.1046 15 14 15.8954 14 17Z"
+                        stroke="currentColor"
+                        strokeWidth="1.5"
+                      />
+                      <path
+                        d="M6.96014 3.69772C5.6417 4.07415 4.69384 4.54112 3.82645 5.10455C2.45318 5.9966 1.86443 7.60404 2.02607 9.15513C2.09439 9.81068 2.62064 10.1241 3.23089 9.95455C3.69451 9.82571 4.15888 9.7003 4.61961 9.56364C5.96706 9.16397 6.28399 8.67812 6.47124 7.29885L6.96014 3.69772ZM6.96014 3.69772C10.2186 2.76743 13.7814 2.76743 17.0399 3.69772M17.0399 3.69772C18.3583 4.07415 19.3062 4.54112 20.1735 5.10455C21.5468 5.9966 22.1356 7.60404 21.9739 9.15513C21.9056 9.81068 21.3794 10.1241 20.7691 9.95455C20.3055 9.82571 19.8411 9.7003 19.3804 9.56364C18.0329 9.16397 17.716 8.67812 17.5288 7.29885L17.0399 3.69772Z"
+                        stroke="currentColor"
+                        strokeWidth="1.5"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  </div>
+                  <div>
+                    <h5 className="mb-[18px] text-lg font-semibold text-dark dark:text-white">
+                    Phone Contact
+                    </h5>
+                    <p className="text-base text-body-color dark:text-dark-6">
+                      <a href="tel:+1 305 234 4234">+1 305 234 4234</a> 
+                    </p>
+                    <p className="text-base text-body-color dark:text-dark-6">
+                      <a href="tel:+1 305 234 4567">+1 305 234 4567</a> 
+                    </p>
+                  </div>
+                </div>
+                <div className="mb-8 flex w-[330px] max-w-full">
+                  <div className="mr-6 text-[32px] text-primary">
+                    <svg
+                      width="34"
+                      height="25"
+                      viewBox="0 0 34 25"
+                      className="fill-current"
+                    >
+                      <path d="M30.5156 0.960938H3.17188C1.42188 0.960938 0 2.38281 0 4.13281V20.9219C0 22.6719 1.42188 24.0938 3.17188 24.0938H30.5156C32.2656 24.0938 33.6875 22.6719 33.6875 20.9219V4.13281C33.6875 2.38281 32.2656 0.960938 30.5156 0.960938ZM30.5156 2.875C30.7891 2.875 31.0078 2.92969 31.2266 3.09375L17.6094 11.3516C17.1172 11.625 16.5703 11.625 16.0781 11.3516L2.46094 3.09375C2.67969 2.98438 2.89844 2.875 3.17188 2.875H30.5156ZM30.5156 22.125H3.17188C2.51562 22.125 1.91406 21.5781 1.91406 20.8672V5.00781L15.0391 12.9922C15.5859 13.3203 16.1875 13.4844 16.7891 13.4844C17.3906 13.4844 17.9922 13.3203 18.5391 12.9922L31.6641 5.00781V20.8672C31.7734 21.5781 31.1719 22.125 30.5156 22.125Z" />
+                    </svg>
+                  </div>
+                  <div>
+                    <h5 className="mb-[18px] text-lg font-semibold text-dark dark:text-white">
+                      How Can We Help?
+                    </h5>
+                    <p className="text-base text-body-color dark:text-dark-6">
+                      info@yourdomain.com
+                    </p>
+                    <p className="mt-1 text-base text-body-color dark:text-dark-6">
+                      contact@yourdomain.com
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="mb-8 flex w-[330px] max-w-full">
-              <div className="mr-6 text-[32px] text-primary">
-                <svg width="34" height="25" viewBox="0 0 34 25" className="fill-current">
-                  <path
-                    d="M30.5156 0.960938H3.17188C1.42188 0.960938 0 2.38281 0 4.13281V20.9219C0 22.6719 1.42188 24.0938 3.17188 24.0938H30.5156C32.2656 24.0938 33.6875 22.6719 33.6875 20.9219V4.13281C33.6875 2.38281 32.2656 0.960938 30.5156 0.960938ZM30.5156 2.875C30.7891 2.875 31.0078 2.92969 31.2266 3.09375L17.6094 11.3516C17.1172 11.625 16.5703 11.625 16.0781 11.3516L2.46094 3.09375C2.67969 2.98438 2.89844 2.875 3.17188 2.875H30.5156ZM30.5156 22.125H3.17188C2.51562 22.125 1.91406 21.5781 1.91406 20.8672V5.00781L15.0391 12.9922C15.5859 13.3203 16.1875 13.4844 16.7891 13.4844C17.3906 13.4844 17.9922 13.3203 18.5391 12.9922L31.6641 5.00781V20.8672C31.7734 21.5781 31.1719 22.125 30.5156 22.125Z" />
-                </svg>
-              </div>
-              <div>
-                <h5 className="mb-[18px] text-lg font-semibold text-dark dark:text-white">How Can We Help?</h5>
-                <p className="text-base text-body-color dark:text-dark-6">info@yourdomain.com</p>
-                <p className="text-base text-body-color dark:text-dark-6 mt-1">
-                  contact@yourdomain.com
-                </p>
-              </div>
-            </div>
           </div>
-        </div>
-      </div>
-      <div className="w-full px-4 lg:w-5/12 xl:w-4/12">
-        {/* <div
+          <div className="w-full px-4 lg:w-5/12 xl:w-4/12">
+            {/* <div
           className="wow fadeInUp rounded-lg bg-white dark:bg-dark-2 py-10 px-8 shadow-testimonial dark:shadow-none sm:py-12 sm:px-10 md:p-[60px] lg:p-10 lg:py-12 lg:px-10 2xl:p-[60px]"
           data-wow-delay=".2s"
         >
@@ -90,10 +126,10 @@ export default function Contact() {
           </form>
         </div> */}
 
-        <FormContact />
+            <FormContact />
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</section>
-  )
+    </section>
+  );
 }
