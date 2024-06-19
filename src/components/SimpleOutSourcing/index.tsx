5f2dd275f3589f70f428ec14d435a2a15eba0750
Ing. Rosdanys Ramirez Arcas <rosdanys@gmail.com>
2024-06-19 18:20:25 -0400

    fix: text

--- src/components/SimpleOutSourcing/index.tsx
+++ src/components/SimpleOutSourcing/index.tsx
@@ -20,19 +20,19 @@ export default function SimpleOutSourcing() {
               />
             </div>
             <div className="mt-6 max-w-xl space-y-3 px-4 sm:px-0 md:mt-0 lg:max-w-2xl">
-              <p className="text-3xl mb-5 font-semibold text-primary sm:text-4xl">
+              <p className="mb-5 text-3xl font-semibold text-primary sm:text-4xl">
                 Talent Acquisition On-Demand
               </p>
               <span className="mt-3 text-body-color dark:text-dark-6">
                 <ul className="mb-10 list-disc">
-                  <li className="mb-2 text-base   text-body-color dark:text-dark- ">
+                  <li className="dark:text-dark- mb-2   text-base text-body-color ">
                     Deliver talent when you need it for a flat monthly fee.
                   </li>
-                  <li className="mb-2 text-base   text-body-color dark:text-dark- ">
+                  <li className="dark:text-dark- mb-2   text-base text-body-color ">
                     If you need a Talent Advisor or a team of Talent Advisors,
                     we can make that happen without a long-term commitment.
                   </li>
-                  <li className="mb-2 text-base   text-body-color dark:text-dark- ">
+                  <li className="dark:text-dark- mb-2   text-base text-body-color ">
                     Each On-Demand resource can be available for different time
                     frames at a fixed monthly rate (3-Month Minimum). This makes
                     budgeting simple and eliminates unexpected costs.
@@ -48,16 +48,21 @@ export default function SimpleOutSourcing() {
           <div className="items-start gap-x-12 sm:px-4 md:px-0 lg:flex">
             <div className="mt-6 max-w-xl space-y-3 px-4 sm:px-0 md:mt-0 lg:max-w-2xl">
               <p className="text-3xl font-semibold text-primary sm:text-4xl">
-              Talent Acquisition On-Demand Types
+                Talent Acquisition On-Demand Options
               </p>
               <span className="mt-3 text-body-color dark:text-dark-6">
-              <h2 className="text-white my-2">Talent Advisor</h2><p className="py-2">{`
+                <h2 className="my-2 text-white">Talent Advisor</h2>
+                <p className="py-2">{`
 Talent Advisors who integrate into your team as if they were your own employees. We will pair you with a Talent Advisor who specializes in your specific hiring needs and can effortlessly adapt to your company culture.
 `}</p>
-                <h2 className="text-white my-2">Sourcer</h2><p className="py-2">{`
+                <h2 className="my-2 text-white">Sourcer</h2>
+                <p className="py-2">{`
 Sourcers who are passionate about candidate searches and consistently provide robust candidate pipelines for you. Our specialized team handles the entire process, delivering qualified candidates for your weekly hiring leader engagements.
 `}</p>
-                <h2 className="text-white my-2">Process Improvement Designer</h2><p className="py-2">{`
+                <h2 className="my-2 text-white">
+                  Process Improvement Designer
+                </h2>
+                <p className="py-2">{`
 Talent consultants who seamlessly integrate with your team to enhance your processes. With intelligent design, we create innovative talent acquisition experiences that deliver results.`}</p>
               </span>
             </div>
